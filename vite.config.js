0a3ec0310c3df498b033ee2072f8f5acf68b2f28
Joao Almeida <joao-almeida11@outlook.com>
2025-06-16 21:00:22 +0100

    feat: pwa partial 01

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,57 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
+
+      devOptions: {
+        enabled: true,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+
+      manifest: {
+        name: "Joao Almeida - Developer",
+        short_name: "Joao Almeida",
+        description: "Personal Website of Joao Almeida, a Web Developer",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        icons: [
+          {
+            src: "favicon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+    }),
+  ],
   base: "/portfolio-2025/",
 });
