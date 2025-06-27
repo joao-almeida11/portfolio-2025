70301b388cf1500df4369a8137f0fcc9d76939b2
Joao Almeida <joao-almeida11@outlook.com>
2025-06-27 05:02:54 +0100

    fix: projects icon

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,11 @@
-import { NotebookPen } from "lucide-react";
+import { FolderKanban } from "lucide-react";
 import ProjectList from "./ProjectList";
 
 export default function Projects() {
   return (
     <section className="projects" id="projects">
       <div className="title-2--container">
-        <NotebookPen color="#608abf" size={34} />
+        <FolderKanban color="#608abf" size={34} />
         <h2 className="title-2">Projects</h2>
       </div>
       <ProjectList />
