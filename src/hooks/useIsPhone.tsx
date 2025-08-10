bc7aa256abe1949f3c556a906622d9dcc6abdb81
Joao Almeida <joao-almeida11@outlook.com>
2025-08-10 03:55:50 +0100

    fix: navbar width switch to collapsed

--- src/hooks/useIsPhone.tsx
+++ src/hooks/useIsPhone.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 
 export default function useIsPhone() {
-  const [isPhone, setIsPhone] = useState(() => window.innerWidth <= 768);
+  // isPhone should be 768px but since this is only used on the navbar and it is quite large i will increase its size
+  const [isPhone, setIsPhone] = useState(() => window.innerWidth <= 800);
 
   useEffect(() => {
     function handleResize() {
-      setIsPhone(window.innerWidth <= 768);
+      setIsPhone(window.innerWidth <= 800);
     }
 
     window.addEventListener("resize", handleResize);
